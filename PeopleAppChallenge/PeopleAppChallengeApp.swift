9f37cbe74b368423d63be40c6e622ffe7674c8f4
John <johnathanlee72@hotmail.com>
2022-04-17 16:46:14 -0700

    Initial Commit

---
+++ PeopleAppChallenge/PeopleAppChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PeopleAppChallengeApp.swift
+//  PeopleAppChallenge
+//
+//  Created by Johnathan Lee on 4/17/22.
+//
+
+import SwiftUI
+
+@main
+struct PeopleAppChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
